21a2f58215856100c34d6389c0e331afee2b5b9e
Mikhail Chernyshov <chernyshov.michaele@icloud.com>
2024-03-13 21:36:47 +0100

    Initial Commit

---
+++ Aeson/AesonApp.swift
@@ -0,0 +1,17 @@
+//
+//  AesonApp.swift
+//  Aeson
+//
+//  Created by Mikhail Chernyshov on 13/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct AesonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
